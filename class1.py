820cbb93150fa5a3e9663e05b216eb941aa8703a
MahamudulHasanAbid <mdabid91221.kh@gmail.com>
2023-12-31 16:23:25 +0600

    first commit

---
+++ class1.py
@@ -0,0 +1,259 @@
+# print("Hello World!")
+# print("Hello World!")
+"""
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+"""
+# print("Hello World!")
+# print("Hello World!")
+
+# id = 100
+# name = "John"
+# Name = "John 1"
+
+# print(id)
+# print(name)
+# print(Name)
+
+# a = 'Hello, World! k'
+
+# b = 20
+
+# c = 20.5
+
+# d = 5564j
+
+# print(a)
+# print(type(a))
+
+# print(b)
+# print(type(b))
+# print(type(c))
+# print(type(d))
+
+
+# x = ["apple", "banana", "cherry"]
+
+# print(x)
+
+# print(type(x)) 
+
+
+# x = ("apple", "banana", "cherry")
+
+# #display x:
+# print(x)
+
+# #display the data type of x:
+# print(type(x)) 
+
+
+# x = range(6)
+
+# print(x)
+
+# print(type(x)) 
+
+# x = {"name" : "John", "age" : 36}
+
+# print(x)
+
+
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist[-1])
+
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[:6])
+
+# thislist = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+# print(thislist[-1:])
+
+# print(type(thislist))
+
+
+
+
+# n = 'Tushar'
+# age = 23
+# print(f"Hello, My name is name and I'm {age} years old.")
+
+# print(f'my name is {n}')
+
+
+# txt = "Hello Horld"
+# txt = txt.replace("H", "J")
+
+# print(txt)
+
+
+# if 5 != 10 and 4 == 4:
+#     print("At least one of the statements is true")
+
+
+# fruits = ["apple", "banana", "cherry"]
+
+# fruits[0:2]= "kiwi", "cherry"
+
+# print(fruits)
+
+
+# fruits = ["apple", "banana", "cherry"]
+# fruits.insert(0,"lemon")
+
+# print(fruits)
+
+
+# fruits = ["apple", "banana", "cherry"]
+# fruits.remove("banana")
+
+# print(fruits)
+
+# fruits = ["apple", "banana", "cherry", "cherry"]
+# print(len(fruits[2:]))
+
+
+
+# car =	{
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# print(car.get("model"))
+# print(car["model"])
+
+
+# x = ("apple", "banana", "cherry")
+# x[0]= 'cherry'
+
+# print(x)
+# print(type(x))
+
+
+# x = ("apple", "banana", "cherry")
+# y = list(x)
+# y[1] = "kiwi"
+# x = tuple(y)
+
+# print(x)
+
+
+# thisset = {"apple", "banana", "cherry"}
+
+# for x in thisset:
+#   print(x)
+
+
+# set1 = {"a", "b" , "c"}
+# set2 = {1, 2, 3}
+
+# set3 = set1.union(set2)
+# print(set3)
+
+
+# x = {"apple", "banana", "cherry"}
+# y = {"google", "microsoft", "apple"}
+
+# z = x.intersection(y)
+
+# print(z)
+
+
+# thisdict = {
+#   'brand': "Ford",
+#   "electric": False,
+#   "year": 1964,
+#   "colors": ["red", "white", "blue"]
+# }
+
+# print(thisdict['colors'][1])
+
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+
+# x = thisdict.keys()
+
+# print(x)
+
+
+# car = {
+# "brand": "Ford",
+# "model": "Mustang",
+# "year": 1964
+# }
+
+
+# print(car) 
+
+# car["year"] = 2020
+
+# print(car) 
+
+
+
+# car = {
+# "brand": "Ford",
+# "model": "Mustang",
+# "year": 1964
+# }
+
+# x = car.values()
+
+# print(x) 
+
+# car["color"] = "red"
+
+# print(x) 
+
+
+# car = {
+# "brand": "Ford",
+# "model": "Mustang",
+# "year": 1964
+# }
+
+# x = car.items()
+
+# print(x) #before the change
+
+# car["color"] = "red"
+
+# print(car) #after the change
+
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# if "model" in thisdict:
+#   print("Yes, 'model' is one of the keys in the thisdict dictionary")
+
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# del thisdict["model"]
+# print(thisdict)
+
+
+# thisdict =	{
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# for x in thisdict.values():
+#   print(x)
+
+
+
+
